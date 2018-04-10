493403f420cd17f0084f77e1bc5f6f53c9ef5d01
andrew malikov <andrew.devels@gmail.com>
2018-04-10 22:27:01 +0500

    Add prototype stroke fill

---
+++ lib/algorithms/stroke_fill.dart
@@ -0,0 +1,103 @@
+import 'dart:collection';
+
+import 'package:GraphicsApp/data/color.dart';
+import 'package:GraphicsApp/math/point.dart';
+import 'package:GraphicsApp/view/bitmap.dart';
+import 'package:GraphicsApp/view/bitmap_utils.dart';
+
+class StrokeFill {
+  Color _color;
+
+  StrokeFill(Color color) {
+    this.color = color;
+  }
+
+  void draw(Bitmap bitmap, Point2D point) {
+    point.round();
+
+    final baseColor = bitmap.getPixel(point.x, point.y, 1);
+    final utils = new BitmapUtils(bitmap);
+    final points = new ListQueue<Point2D>();
+
+    points.add(point);
+
+    while (points.isNotEmpty) {
+      final point = points.removeFirst();
+      var x = point.x;
+      var y = point.y;
+      var colorLine = utils.getHorizontalLine(y);
+
+      bitmap.setPixel(x, y, 1, color);
+
+      var rightX;
+      var leftX = x;
+
+      x++;
+
+      while (x < bitmap.width && _getColor(colorLine, x) == baseColor) {
+        bitmap.setPixel(x, y, 1, color);
+        x++;
+      }
+
+      rightX = x-1;
+      x = leftX - 1;
+
+      while (x >= 0 && _getColor(colorLine, x) == baseColor) {
+        bitmap.setPixel(x, y, 1, color);
+        x--;
+      }
+
+      leftX = x + 1;
+      if (y + 1 >= 0) {
+        colorLine = utils.getHorizontalLine(y + 1);
+        var intervals = _getIntervals(colorLine, leftX, rightX, baseColor);
+        if (intervals.length % 2 != 0) intervals = [];
+
+        for (var i = 1; i < intervals.length; i += 2)
+          points.addLast(new Point2D(intervals[i], y + 1));
+      }
+
+      if (y - 1 <= bitmap.height) {
+        colorLine = utils.getHorizontalLine(y - 1);
+        var intervals = _getIntervals(colorLine, leftX, rightX, baseColor);
+        if (intervals.length % 2 != 0) intervals = [];
+
+        for (var i = 1; i < intervals.length; i += 2)
+          points.addLast(new Point2D(intervals[i], y - 1));
+      }
+    }
+  }
+
+  Color _getColor(List<int> colorLine, num number) {
+    // if (colorLine.length / 4 <= number)
+    //  number = number.round() % (colorLine.length / 4);
+    final channels = colorLine.sublist(number * 4, number * 4 + 4);
+    return ColorBuilder.RGB(channels[0], channels[1], channels[2]);
+  }
+
+  List<num> _getIntervals(
+      List<int> colorLine, int from, int to, Color baseColor) {
+    var intervals = new List<int>();
+
+    var color = _getColor(colorLine, from);
+    if (color == baseColor) intervals.add(from);
+
+    for (var i = from; i < to; i++) {
+      var nextColor = _getColor(colorLine, i + 1);
+      if (color == baseColor && nextColor != baseColor) {
+        intervals.add(i);
+        color = nextColor;
+      } else if (color != baseColor && nextColor == baseColor) {
+        intervals.add(i + 1);
+        color = nextColor;
+      }
+    }
+
+    if (_getColor(colorLine, to) == baseColor) intervals.add(to);
+
+    return intervals;
+  }
+
+  Color get color => _color;
+  void set color(Color color) => _color = color;
+}
---
+++ lib/tools/stroke_fill_tool.dart
@@ -0,0 +1,35 @@
+import 'dart:html';
+
+import 'package:GraphicsApp/algorithms/stroke_fill.dart';
+import 'package:GraphicsApp/data/icons.dart';
+import 'package:GraphicsApp/math/point.dart';
+import 'package:GraphicsApp/tools/tool.dart';
+
+class StrokeFillTool extends Tool {
+  StrokeFill _tool;
+
+  StrokeFillTool(StrokeFill tool)
+      : super(new ToolMetadata('fill', Icons['bucket'])) {
+    this.tool = tool;
+  }
+
+  @override
+  void registrateEvents() {
+    events['click'] = _debugClick;
+  }
+
+  void _click(Event event) {
+    window.requestAnimationFrame((time) {
+      Point2D point = computePoint(event as MouseEvent, layer.body);
+      tool.draw(layer.body, point);
+    });
+  }
+
+  void _debugClick(Event event) {
+    Point2D point = computePoint(event as MouseEvent, layer.body);
+    tool.draw(layer.body, point);
+  }
+
+  StrokeFill get tool => _tool;
+  void set tool(StrokeFill tool) => _tool = tool;
+}
